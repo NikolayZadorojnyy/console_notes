98db7d604e49fa2ce2278bdd4e0b278ba6fddb2c
Nikolay Zadorojnyy <znikolay69@gmail.com>
2023-06-08 01:26:29 +0300

    тестовый запуск

--- model/note.py
+++ model/note.py
@@ -1,13 +1,10 @@
 from datetime import datetime
-
-
 class Note:
-
     def __init__(self, id, title, content):
         self.id = id
         self.title = title
         self.content = content
-        self.date = datetime.now().strftime('%Y, %B %d, %A | %H:%M')
+        self.date = datetime.now().strftime('%Y %B %d %A | %H:%M')
 
     def __str__(self) -> str:
-        return f"Заметка №{self.id}\nдата: {self.date}\n{self.title}\n{self.content}"
+        return f"Заметка №{self.id}\nДата: {self.date}\nЗаголовок: {self.title}\nСодержимое: {self.content}"
